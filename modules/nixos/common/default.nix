d8d1617aa79f1c8c3e0767693e5568b3143c8ef9
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2025-10-31 11:17:12 +0100

    feat: add docker

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -1,3 +1,10 @@
-{...}: {
+{lib, ...}: let
+  inherit (lib) mkDefault;
+in {
   imports = [./nix.nix ./nixpkgs.nix ./programs.nix];
+
+  virtualisation.docker = {
+    enable = mkDefault true;
+    enableOnBoot = mkDefault false;
+  };
 }
