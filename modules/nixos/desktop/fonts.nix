753b08f70da7df71a291506eb9a152da8f2feaae
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2025-11-05 23:41:17 +0100

    feat: add japanese font

--- modules/nixos/desktop/fonts.nix
+++ modules/nixos/desktop/fonts.nix
@@ -1,20 +1,35 @@
-{pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    nerd-fonts.jetbrains-mono
-    nerd-fonts.geist-mono
+{
+  pkgs,
+  config,
+  ...
+}: let
+  fonts = config.stylix.fonts;
+in {
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.geist-mono
 
-    eb-garamond
-    fira-sans
-    montserrat
-    noto-fonts
-    open-sans
-    roboto
-    source-sans-pro
+      eb-garamond
+      fira-sans
+      montserrat
+      noto-fonts
+      open-sans
+      roboto
+      source-sans-pro
+      noto-fonts-cjk-sans
 
-    noto-fonts-color-emoji
-    openmoji-black
-    openmoji-color
-    twemoji-color-font
-    twitter-color-emoji
-  ];
+      noto-fonts-color-emoji
+      openmoji-black
+      openmoji-color
+      twemoji-color-font
+      twitter-color-emoji
+    ];
+
+    fontconfig.defaultFonts = {
+      sansSerif = [fonts.sansSerif.name "Noto Sans CJK JP"];
+      serif = [fonts.sansSerif.name "Noto Sans CJK JP"];
+      monospace = [fonts.monospace.name "Noto Sans Mono CJK JP"];
+    };
+  };
 }
