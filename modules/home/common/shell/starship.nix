2717de16a6c05864e700c270ddd19ec4d5b6bae4
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2025-11-19 23:23:29 +0100

    feat: configure starship prompt

--- modules/home/common/shell/starship.nix
+++ modules/home/common/shell/starship.nix
@@ -4,15 +4,40 @@
   ...
 }: let
   cfg = config.programs.starship;
-  inherit (lib) mkIf;
+  inherit (lib) mkIf concatStrings;
 in {
   config = mkIf cfg.enable {
     programs.starship = {
       settings = {
+        format = concatStrings [
+          "$os"
+          "$directory"
+          "$character"
+        ];
+        right_format = concatStrings [
+          "$git_branch"
+          "$git_status"
+          "$git_state"
+          "$nix_shell"
+          "$line_break"
+        ];
         add_newline = true;
-        aws.disabled = true;
-        gcloud.disabled = true;
-        line_break.disabled = true;
+
+        git_branch.format = "[$symbol$branch(:$remote_branch)]($style) ";
+        nix_shell = {
+          format = "[$symbol]($style)";
+          symbol = "❄️";
+        };
+        os.disabled = false;
+
+        character = {
+          success_symbol = "[](bold green)";
+          error_symbol = "[](bold red)";
+          vimcmd_symbol = "[](bold green)";
+          vimcmd_replace_one_symbol = "[](bold purple)";
+          vimcmd_replace_symbol = "[](bold purple)";
+          vimcmd_visual_symbol = "[](bold yellow)";
+        };
       };
     };
   };
