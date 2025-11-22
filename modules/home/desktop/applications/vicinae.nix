90f360b5ddfd8fe04b5c71830ecfceb1f3a981c7
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2025-11-22 12:17:37 +0100

    chore: update flake

--- modules/home/desktop/applications/vicinae.nix
+++ modules/home/desktop/applications/vicinae.nix
@@ -1,8 +1,6 @@
 {
   config,
   lib,
-  inputs,
-  pkgs,
   ...
 }: let
   inherit (lib) mkIf;
@@ -33,21 +31,6 @@ in {
           rounding = 10;
         };
       };
-
-      extensions = [
-        (inputs.vicinae.mkVicinaeExtension.${pkgs.stdenv.hostPlatform.system} {
-          inherit pkgs;
-          name = "awww-switcher";
-          src = pkgs.fetchFromGitHub {
-            # You can also specify different sources other than github
-            owner = "lorenzorapetti";
-            repo = "awww-switcher";
-            rev = "a8539bf44faefe6652b5d1ab25f3f06fa78f607a"; # If the extension has no releases use the latest commit hash
-            # You can get the sha256 by rebuilding once and then copying the output hash from the error message
-            sha256 = "sha256-3FT1j9xTF/72hfzgJnR1ebPRLccOYhOSIQuCI3IxqUo=";
-          }; # If the extension is in a subdirectory you can add ` + "/subdir"` between the brace and the semicolon here
-        })
-      ];
     };
   };
 }
