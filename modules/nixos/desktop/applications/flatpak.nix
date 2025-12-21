4b831fbc6ae2b20d3fd767a9a65822149235ffa0
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2025-12-21 21:17:14 +0100

    feat: add flatpak remotes

--- modules/nixos/desktop/applications/flatpak.nix
+++ modules/nixos/desktop/applications/flatpak.nix
@@ -6,17 +6,19 @@
 }: {
   services.flatpak = {
     enable = lib.mkDefault true;
+    remotes = [
+      {
+        name = "flathub";
+        location = "https://flathub.org/repo/flathub.flatpakrepo";
+      }
+      {
+        name = "flathub-beta";
+        location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+      }
+    ];
     packages = [
       "com.bambulab.BambuStudio"
     ];
-  };
-
-  systemd.services.flatpak-repo = lib.mkIf config.services.flatpak.enable {
-    wantedBy = ["multi-user.target"];
-    path = [pkgs.flatpak];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
   };
 
   environment.systemPackages = lib.mkIf config.services.flatpak.enable [
