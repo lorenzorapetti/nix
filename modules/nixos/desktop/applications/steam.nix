a67f67bcbf57e2b46c8d000025a100d9c903c38a
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2026-01-18 18:54:20 +0100

    feat: add steam firewall rules

--- modules/nixos/desktop/applications/steam.nix
+++ modules/nixos/desktop/applications/steam.nix
@@ -2,6 +2,9 @@
   # TODO: Improve Steam performance
   programs.steam = {
     enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     package = pkgs.steam.override {
       extraArgs = "-system-composer";
     };
