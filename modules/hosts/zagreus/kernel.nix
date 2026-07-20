7e1662d31b5df00bad752cb3b8de94247f4d7f42
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2026-07-20 16:10:42 +0200

    fix cachyos

--- modules/hosts/zagreus/kernel.nix
+++ modules/hosts/zagreus/kernel.nix
@@ -5,19 +5,13 @@
     ];
 
     nixos = {pkgs, ...}: {
-      kernel = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto-zen4.override = {
-        performanceGovernor = true;
-        bbr3 = true;
-      };
+      boot.kernelPackages = let
+        kernel = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto-zen4.override = {
+          performanceGovernor = true;
+          bbr3 = true;
+        };
 
-      kernelPackages = pkgs.linuxKernel.packagesFor kernel;
-
-      # For LTO kernels, helpers.kernelModuleLLVMOverride fixes compilation for some
-      # out-of-tree modules in nixpkgs.
-      kernelPackagesWithLTOFix = let
-        # helpers.nix provides a few utilities for building kernel with LTO.
-        # I haven't figured out a clean way to expose it in flakes.
-        helpers = pkgs.callPackage "${inputs.nix-cachyos-kernel.outPath}/helpers.nix" {};
+        helpers = pkgs.callPackage "${inputs.nix-cachyos-kernel.outPath}/helpers.nix" { };
       in helpers.kernelModuleLLVMOverride (pkgs.linuxKernel.packagesFor kernel);
     };
   };
