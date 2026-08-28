e50ac1d7880671260c4d31b68d81864122d77952
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2026-08-28 18:03:22 +0200

    add btrbk snapshot

--- modules/hardware/btrfs.nix
+++ modules/hardware/btrfs.nix
@@ -7,6 +7,19 @@
       };
 
       services.smartd.enable = true;
+
+      services.btrbk.instances.local = {
+        onCalendar = "daily";
+        snapshotOnly = true;
+        settings = {
+          snapshot_preserve_min = "2d";
+          snapshot_preserve = "7d";
+          volume."/" = {
+            subvolume = ".";
+            snapshot_dir = "/.snapshots";
+          };
+        };
+      };
     };
   };
 }
