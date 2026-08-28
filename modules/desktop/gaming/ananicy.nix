1c9f1c830f8fec3a9a947594698a218a6e238608
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2026-08-28 17:39:13 +0200

    re-enable ananicy-cpp

--- modules/desktop/gaming/ananicy.nix
+++ modules/desktop/gaming/ananicy.nix
@@ -1,13 +1,11 @@
 {
   den.aspects.desktop.gaming = {
     nixos = {pkgs, ...}: {
-      # TODO: Reenable this when https://github.com/NixOS/nixpkgs/pull/552211 gets merged.
-      #
-      # services.ananicy = {
-      #   enable = true;
-      #   package = pkgs.ananicy-cpp;
-      #   rulesProvider = pkgs.ananicy-rules-cachyos;
-      # };
+      services.ananicy = {
+        enable = true;
+        package = pkgs.ananicy-cpp;
+        rulesProvider = pkgs.ananicy-rules-cachyos;
+      };
 
       fileSystems."/sys/kernel/tracing" = {
         device = "tracefs";
